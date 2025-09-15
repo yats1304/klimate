1fc16dbe8db953a2c6eafeadedf7f7f2bb6587c9
yats1304 <chaubaly@gmail.com>
2025-09-15 20:07:24 +0530

    weather detail card added

---
+++ src/components/weather-details.tsx
@@ -0,0 +1,83 @@
+import type { WeatherData } from "@/api/types";
+import { Compass, Gauge, Sunrise, Sunset } from "lucide-react";
+import { format } from "date-fns";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+interface WeatherDetailsProps {
+  data: WeatherData;
+}
+
+const WeatherDetails = ({ data }: WeatherDetailsProps) => {
+  const { wind, main, sys } = data;
+
+  const getWindDirection = (degree: number) => {
+    const direction = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+
+    const index =
+      Math.round(((degree %= 360) < 0 ? degree + 360 : degree) / 45) % 8;
+    return direction[index];
+  };
+
+  const formatTime = (timestamp: number) => {
+    return format(new Date(timestamp * 1000), "h:m a");
+  };
+
+  const details = [
+    {
+      title: "Sunrise",
+      value: formatTime(sys.sunrise),
+      icon: Sunrise,
+      color: "text-orange-500",
+    },
+    {
+      title: "Sunset",
+      value: formatTime(sys.sunset),
+      icon: Sunset,
+      color: "text-blue-500",
+    },
+    {
+      title: "Wind Direction",
+      value: `${getWindDirection(wind.deg)} (${wind.deg})°`,
+      icon: Compass,
+      color: "text-green-500",
+    },
+    {
+      title: "Pressure",
+      value: `${main.pressure} hPa`,
+      icon: Gauge,
+      color: "text-purple-500",
+    },
+  ];
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Weather Details</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-6 sm:grid-cols-2">
+          {details.map((detail) => {
+            return (
+              <div
+                key={detail.title}
+                className="flex items-center gap-3 rounded-lg border p-4"
+              >
+                <detail.icon className={`h-5 w-5 ${detail.color}`} />
+                <div>
+                  <p className="text-sm font-medium leading-none">
+                    {detail.title}
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {detail.value}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default WeatherDetails;
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,6 +3,7 @@ import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import WeatherDetails from "@/components/weather-details";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import {
   UseForecastQuery,
@@ -120,6 +121,7 @@ const WeatherDashboard = () => {
 
         <div>
           {/* detail */}
+          <WeatherDetails data={weatherQuery.data} />
           {/* forecast */}
         </div>
       </div>
